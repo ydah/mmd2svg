2804e4bf0852d01a33c41e38f62e75b1dab517da
ydah <t.yudai92@gmail.com>
2025-10-29 11:10:53 +0900

    Initial commit

---
+++ lib/mmd2svg.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mmd2svg/version"
+
+module Mmd2svg
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mmd2svg/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Mmd2svg
+  VERSION = "0.1.0"
+end
